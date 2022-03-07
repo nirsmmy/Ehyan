9831e8f003fd87bf4694f576b1acb07367f91cb1
Neiro Somemiya <smmy@neiro.work>
2022-03-07 15:54:55 +0900

    準備

---
+++ .dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+class Ehyan {
+    constructor() { }
+    start() {
+        console.log('hello world');
+    }
+}
+const ehyan = new Ehyan();
+ehyan.start();
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+class Ehyan {
+  constructor() {}
+  public start(): void {
+    console.log('hello world')
+  }
+}
+
+const ehyan = new Ehyan()
+ehyan.start()
